9de170b7fe7e09c8a9fbfab24249d969472ddbf9
andres <af.hidalgo.romero@gmail.com>
2025-04-07 17:26:10 +0200

    use of utilitiesB

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import utilitiesA as utilA
+import utilitiesB as utilB
 
 
 def main():
@@ -7,6 +8,9 @@ def main():
 
     c = utilA.sumaA(a, b)
     print(f"la suma de a + b es: {c}")
+
+    d = utilB.restaB(a, b)
+    print(f"la resta de a + b es: {d}")
 
 if __name__ == "__main__":
     main()
